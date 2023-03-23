df8397bff1348945328118321eb2f7ce6a495f5e
sonnonet <korea.sonnonet@gmail.com>
2023-03-23 18:28:04 +0900

    Add files via upload

---
+++ c_4/hello.c
@@ -0,0 +1,19 @@
+1
+#include <stdio.h>
+
+
+int main(void)
+{
+    int x;
+    int y;
+    printf("첫번째 숫자를 입력하세요:\n");
+    scanf_s("%d", &x);
+    printf("두번째 숫자를 입력하세요:");
+    scanf_s("%d", &y);
+    printf("%d %d",x,y);
+
+   
+    //int a ;
+    return 0;
+   
+}
